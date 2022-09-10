afcf070a16a401c04f0712f506dc89fbf49b7780
Awais Kayani <akayani976@gmail.com>
2022-09-10 14:25:34 +0500

    Settings Page

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -27,6 +27,7 @@ class _HomePageState extends State<HomePage> {
     int percent = 59;
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.black,
         toolbarHeight: AppLayout.getHeight(130),
         title: Center(
@@ -48,151 +49,153 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: AppLayout.getHeight(360),
-            color: Colors.white,
-            padding: EdgeInsets.only(
-                top: AppLayout.getHeight(35),
-                left: AppLayout.getWidth(15),
-                right: AppLayout.getWidth(15)),
-            child: Center(
-              child: Column(
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                          padding: EdgeInsets.all(AppLayout.getHeight(10)),
-                          child: CircularPercentIndicator(
-                            radius: AppLayout.getHeight(100),
-                            lineWidth: AppLayout.getHeight(12),
-                            animation: true,
-                            percent: percent / 100,
-                            center: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'BATTERY',
-                                  style: TextStyle(
-                                    fontSize: AppLayout.getHeight(16),
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: AppLayout.getHeight(360),
+              color: Colors.white,
+              padding: EdgeInsets.only(
+                  top: AppLayout.getHeight(35),
+                  left: AppLayout.getWidth(15),
+                  right: AppLayout.getWidth(15)),
+              child: Center(
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                            padding: EdgeInsets.all(AppLayout.getHeight(10)),
+                            child: CircularPercentIndicator(
+                              radius: AppLayout.getHeight(100),
+                              lineWidth: AppLayout.getHeight(12),
+                              animation: true,
+                              percent: percent / 100,
+                              center: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'BATTERY',
+                                    style: TextStyle(
+                                      fontSize: AppLayout.getHeight(16),
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  '$percent%',
-                                  style: TextStyle(
-                                    fontSize: AppLayout.getHeight(50),
-                                    fontWeight: FontWeight.w200,
-                                    color: Colors.black,
+                                  Text(
+                                    '$percent%',
+                                    style: TextStyle(
+                                      fontSize: AppLayout.getHeight(50),
+                                      fontWeight: FontWeight.w200,
+                                      color: Colors.black,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            backgroundColor: Colors.grey,
-                            circularStrokeCap: CircularStrokeCap.round,
-                            progressColor:
-                                const Color.fromARGB(255, 128, 242, 88),
-                          )),
-                      Gap(AppLayout.getHeight(28)),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                            vertical: AppLayout.getHeight(5)),
-                        height: AppLayout.getHeight(65),
-                        width: AppLayout.getWidth(350),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.black,
+                                ],
+                              ),
+                              backgroundColor: Colors.grey,
+                              circularStrokeCap: CircularStrokeCap.round,
+                              progressColor:
+                                  const Color.fromARGB(255, 128, 242, 88),
+                            )),
+                        Gap(AppLayout.getHeight(28)),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: AppLayout.getHeight(5)),
+                          height: AppLayout.getHeight(65),
+                          width: AppLayout.getWidth(350),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.black,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.bluetooth,
+                                color: Colors.white,
+                                size: AppLayout.getHeight(45),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Paired with',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
+                                  Text(
+                                    "Aprils's VMARS",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.bluetooth,
-                              color: Colors.white,
-                              size: AppLayout.getHeight(45),
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  'Paired with',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
-                                ),
-                                Text(
-                                  "Aprils's VMARS",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Gap(AppLayout.getHeight(12)),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: AppLayout.getHeight(262),
-            width: AppLayout.getWidth(390),
-            color: const Color.fromARGB(255, 213, 218, 224),
-            child: Center(
-              child: Column(
-                children: [
-                  Gap(AppLayout.getHeight(20)),
-                  GestureDetector(
-                    onTap: isTapped == true
-                        ? null
-                        : () {
-                            setState(() {
-                              isTapped = !isTapped;
-                              Timer(
-                                duration,
-                                () {
-                                  setState(() {
-                                    isTapped = !isTapped;
-                                  });
-                                },
-                              );
-                            });
-                          },
-                    child: AppButton(
-                      text: isTapped == false
-                          ? 'Snooze 5 Minutes'
-                          : 'Snoozed until $snoozeTime',
-                      color: isTapped == false
-                          ? Colors.white
-                          : const Color.fromARGB(255, 240, 159, 0),
-                      textColor:
-                          isTapped == false ? Colors.black54 : Colors.white,
+                        Gap(AppLayout.getHeight(12)),
+                      ],
                     ),
-                  ),
-                  Gap(AppLayout.getHeight(10)),
-                  GestureDetector(
-                    onTap: () {
-                      Get.to(const RecordLogsPage());
-                    },
-                    child: const AppButton(text: 'Record Log'),
-                  ),
-                  Gap(AppLayout.getHeight(10)),
-                  GestureDetector(
-                    onTap: () {
-                      Get.to(const SettingsPage());
-                    },
-                    child: const AppButton(text: 'Settings'),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            Container(
+              height: AppLayout.getHeight(262),
+              width: AppLayout.getWidth(390),
+              color: const Color.fromARGB(255, 213, 218, 224),
+              child: Center(
+                child: Column(
+                  children: [
+                    Gap(AppLayout.getHeight(20)),
+                    GestureDetector(
+                      onTap: isTapped == true
+                          ? null
+                          : () {
+                              setState(() {
+                                isTapped = !isTapped;
+                                Timer(
+                                  duration,
+                                  () {
+                                    setState(() {
+                                      isTapped = !isTapped;
+                                    });
+                                  },
+                                );
+                              });
+                            },
+                      child: AppButton(
+                        text: isTapped == false
+                            ? 'Snooze 5 Minutes'
+                            : 'Snoozed until $snoozeTime',
+                        color: isTapped == false
+                            ? Colors.white
+                            : const Color.fromARGB(255, 240, 159, 0),
+                        textColor:
+                            isTapped == false ? Colors.black54 : Colors.white,
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(10)),
+                    GestureDetector(
+                      onTap: () {
+                        Get.to(const RecordLogsPage());
+                      },
+                      child: const AppButton(text: 'Record Log'),
+                    ),
+                    Gap(AppLayout.getHeight(10)),
+                    GestureDetector(
+                      onTap: () {
+                        Get.to(const SettingsPage());
+                      },
+                      child: const AppButton(text: 'Settings'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/settings_screen.dart
+++ lib/views/settings_screen.dart
@@ -1,13 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:vmar/AppLayout/button.dart';
+import 'package:vmar/AppLayout/getx.dart';
+import 'package:vmar/views/home_screen.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Center(child: Text("Settings")),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        toolbarHeight: AppLayout.getHeight(120),
+        title: Center(
+          child: Column(
+            children: [
+              Text(
+                'PREVAIL TACTICAL',
+                style: TextStyle(fontSize: AppLayout.getHeight(20)),
+              ),
+              Text(
+                'VMARS',
+                style: TextStyle(fontSize: AppLayout.getHeight(70)),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color.fromARGB(255, 213, 218, 224),
+          height: AppLayout.getHeight(630),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: AppLayout.getWidth(20)),
+                    height: AppLayout.getHeight(70),
+                    child: GestureDetector(
+                      onTap: () {
+                        Get.to(const HomePage());
+                      },
+                      child: const Icon(
+                        Icons.arrow_back_ios,
+                        color: Color.fromARGB(255, 150, 155, 162),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: AppLayout.getWidth(300),
+                    height: AppLayout.getHeight(70),
+                    child: const Center(
+                        child: Text(
+                      'Settings',
+                      style: TextStyle(
+                          fontSize: 28,
+                          color: Color.fromARGB(255, 150, 155, 162)),
+                    )),
+                  )
+                ],
+              ),
+              Container(
+                padding:
+                    EdgeInsets.symmetric(horizontal: AppLayout.getWidth(40)),
+                child: Column(
+                  children: [
+                    Gap(AppLayout.getHeight(20)),
+                    const AppButton(text: 'Pair Bluetooth'),
+                    Gap(AppLayout.getHeight(15)),
+                    const AppButton(text: 'Audio'),
+                    Gap(AppLayout.getHeight(15)),
+                    const AppButton(text: 'Alerts'),
+                    Gap(AppLayout.getHeight(15)),
+                    const AppButton(text: 'Training Mode'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
